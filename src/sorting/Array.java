ce7ebe04e38ba2de0de632a726aeb35783fead76
Yash <yash1997bm@gmail.com>
2020-10-05 12:28:54 +0530

    Array.java update

--- src/sorting/Array.java
+++ src/sorting/Array.java
@@ -1,4 +1,24 @@
 package sorting;
 
+import java.util.Scanner;
+
 public class Array {
+    int a[];
+    int n;
+
+    void getData(){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the number of ele");
+        n=s.nextInt();
+        a=new int[n];
+        System.out.println("Enter the "+n+" elements");
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+    }
+
+    void display(int a[],int n){
+        for(int i=0;i<n;i++)
+            System.out.println(a[i]);
+    }
 }
--- src/sorting/Bubble_sort.java
+++ src/sorting/Bubble_sort.java
@@ -1,5 +1,25 @@
 package sorting;
 
 public class Bubble_sort {
+    void bubbleSort(int a[],int n){
+        for(int k=0;k<n-1;k++){
+            for(int i=0;i<n-k-1;i++){
+                if(a[i]>a[i+1]){
+                    int temp = a[i];
+                    a[i]=a[i+1];
+                    a[i+1]=temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        Array r=new Array();
+        Bubble_sort b=new Bubble_sort();
+        r.getData();
+        b.bubbleSort(r.a,r.n);
+        System.out.println("Sorted array");
+        r.display(r.a,r.n);
+    }
 
 }
